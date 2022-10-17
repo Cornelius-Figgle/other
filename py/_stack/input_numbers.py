456c896f8eb23e0058c676c272ddc1231a56a242
Cornelius-Figgle <max@fullimage.net>
2022-10-17 17:47:13 +0100

    did a thing

---
+++ py/_stack/input_numbers.py
@@ -0,0 +1,15 @@
+
+if __name__ == '__main__':
+    first_input = input('Input some letters: ')
+    first_list = first_input.split(' ')
+    
+    second_input = input('Input some numbers: ')
+    second_list = second_input.split(' ')
+    for num in second_list:
+        second_list[second_list.index(num)] = int(num)
+        
+    third_list = []
+    for num in second_list:
+        third_list.append(first_list[num])
+        
+    print(third_list)
