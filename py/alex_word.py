f077c62560df45d05b49e8ffb16fa13f14bbef88
Cornelius-Figgle <max@fullimage.net>
2022-11-25 21:43:52 +0000

    init

---
+++ py/alex_word.py
@@ -0,0 +1,27 @@
+# pebis
+# -*- coding: UTF-8 -*-
+
+# https://github.com/Cornelius-Figgle/other
+
+'''
+A random quote generator for Alex
+
+Pulls quotes from `quotes.txt` which is actually a `csv` file
+'''
+
+# note: view associated GitHub info as well
+__version__ = 'Pre-release'  
+__author__ = 'Cornelius-Figgle'
+__email__ = 'max@fullimage.net'
+__maintainer__ = 'Cornelius-Figgle'
+__copyright__ = 'Copyright (c) 2022 Max Harrison'
+__license__ = 'MIT'
+__status__ = 'Development'
+__credits__ = ['Max Harrison', 'Alex Ceaton', 'Ashe~ Ceaton']
+
+
+def main():
+	print('Hello World')
+
+if __name__ == '__main__':
+	main()
